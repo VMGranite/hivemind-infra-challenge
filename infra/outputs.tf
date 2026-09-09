2cf1080bc5ed061649cd9677793969f64e78dd93
Veronica <VMGranite@gmail.com>
2026-09-09 11:55:25 +0200

    update: ECR Workflow login and image tagging

---
+++ infra/outputs.tf
@@ -0,0 +1,24 @@
+output "ecr_repository_name" {
+  description = "Name of the ECR repository"
+  value       = module.ecr.repository_name
+}
+
+output "ecr_repository_url" {
+  description = "Full URL of the ECR repository, e.g. <account-id>.dkr.ecr.<region>.amazonaws.com/<name>"
+  value       = module.ecr.repository_url
+}
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets (EKS worker nodes)"
+  value       = aws_subnet.private[*].id
+}
