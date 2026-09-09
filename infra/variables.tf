0a187a4b468909aefd2fc2978e31ce31d1750aec
Veronica <VMGranite@gmail.com>
2026-09-09 13:07:45 +0200

    add: az_count variable

--- infra/variables.tf
+++ infra/variables.tf
@@ -16,6 +16,13 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "az_count" {
+  description = "Number of availability zones to spread subnets across"
+  type        = number
+  default     = 2
+}
+
 variable "ecr_access_principal_arn" {
   description = "ARN of the IAM principal (CI role or local user) that needs read/write access to the ECR repository"
   type        = string
