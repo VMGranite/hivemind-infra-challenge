e85019d1fc016141985900a29c4b4f7ff9db57ce
Veronica <VMGranite@gmail.com>
2026-09-09 11:59:29 +0200

    test: Comment out unused Outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -8,17 +8,17 @@ output "ecr_repository_url" {
   value       = module.ecr.repository_url
 }
 
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.main.id
-}
+# output "vpc_id" {
+#   description = "ID of the VPC"
+#   value       = aws_vpc.main.id
+# }
 
-output "public_subnet_ids" {
-  description = "IDs of the public subnets"
-  value       = aws_subnet.public[*].id
-}
+# output "public_subnet_ids" {
+#   description = "IDs of the public subnets"
+#   value       = aws_subnet.public[*].id
+# }
 
-output "private_subnet_ids" {
-  description = "IDs of the private subnets (EKS worker nodes)"
-  value       = aws_subnet.private[*].id
-}
+# output "private_subnet_ids" {
+#   description = "IDs of the private subnets (EKS worker nodes)"
+#   value       = aws_subnet.private[*].id
+# }
