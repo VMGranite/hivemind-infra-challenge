a68d82ec3ca9e3058d998da4a1ca266970a46541
Veronica <VMGranite@gmail.com>
2026-09-13 19:17:56 +0200

    fix: prevent CI from attempting to give itself admin permission

--- infra/eks.tf
+++ infra/eks.tf
@@ -2,7 +2,8 @@ resource "aws_eks_cluster" "eks_cluster_auto_mode" {
   name = local.cluster_name
 
   access_config {
-    authentication_mode = "API"
+    authentication_mode                         = "API"
+    bootstrap_cluster_creator_admin_permissions = false
   }
 
   role_arn = aws_iam_role.cluster.arn
